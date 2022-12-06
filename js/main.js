5244540e1d61bf44785e66afb698871c1eb7906d
Kamedin <eltru@gmx.de>
2022-12-06 19:19:14 +0100

    3d-sidebar items ready at #planning-phase

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Animations to fade the headlines in:
 let headlines = document.querySelectorAll("#header-headlines h1");
 for(let i = 0 ; i < headlines.length ; i++)
 {
@@ -31,12 +32,12 @@ for(let i = 0 ; i < headlines.length ; i++)
             break;
     }
 }
-
+//animation for fading in my picture
 let picHeader = document.getElementById("header-picture-wrapper");
 setTimeout(() => {
     picHeader.style.opacity = "1";
 }, 300);
-
+//animation for changing words(BLOG, PORTFOLIO, WEBSITE):
 let wordChange = document.querySelector(".changing-headline");
 setTimeout(() => {
     wordChange.innerText = "BLOG";
@@ -44,13 +45,280 @@ setTimeout(() => {
 setTimeout(() => {
     wordChange.innerText = "WEBSITE";
 }, 1500);
-
+//animation for dafing in the qestionMark:
 let qmark = document.getElementById("qMark");
 setTimeout(() => {
     qmark.style.opacity = "1";
 }, 1600);
-
+// animation for fading in the initial infos at my picture:
 let iInfo = document.getElementById("initial-info");
 setTimeout(() => {
     iInfo.style.opacity = "1";
 }, 300);
+
+//toggles the color of the links within the main nav:
+let allLinks = document.querySelectorAll("nav ul li a");
+console.log(allLinks);
+allLinks.forEach(l => {
+    l.addEventListener("click" , e => {
+        for(let i = 0 ; i < allLinks.length ; i ++)
+        {
+            //first remove orage color on every svg
+            allLinks[i].classList.remove("activeLink");
+            if(i !== 4)
+            {
+                allLinks[i].children[0].style.fill = "#999";
+            }
+            else
+            {
+                allLinks[i].children[0].style.stroke = "#999";
+            }
+            //then add orange color to the clocked one:
+            if(e.target.innerText === "TECH STACK")
+            {
+                e.target.children[0].style.stroke = "var(--tert1)";
+            }
+            else
+            {
+                e.target.children[0].style.fill = "var(--tert1)";
+            }
+        }
+        e.target.classList.add("activeLink");
+
+        e.target.children[0].children[0].removeAttribute("stroke");
+        
+    });
+});
+
+let sidebars = document.querySelectorAll(".sidebar-card");
+
+
+//rotationY exists between 5 and 15deg
+sidebars.forEach(card => {
+    card.addEventListener("mousemove", e => {
+        console.clear();
+        console.log(e.target.clientWidth);
+        console.log(e.offsetX);
+        
+        let halfOfWidthX = e.target.clientWidth / 2;
+        let halfOfWidthY = e.target.clientHeight / 2;
+        let tenthOfWidthX = (halfOfWidthX / 10);
+        let tenthOfWidthY = (halfOfWidthY / 10);
+
+        console.clear();
+        e.target.style.transform = `rotateY(${_computeXRotation(e.offsetX, e.target.clientWidth)}deg) rotateX(${_computeYRotation(e.offsetY, e.target.clientHeight)}deg)`;
+        // if(e.offsetX > halfOfWidthX)
+        // {
+        //     if(e.offsetX > halfOfWidthX && e.offsetX < halfOfWidthX + (1 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(5deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (1 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (2 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(6deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (2 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (3 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(7deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (3 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (4 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(8deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (4 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (5 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(9deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (5 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (6 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(10deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (6 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (7 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(11deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (7 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (8 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(12deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (8 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (9 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(13deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthX + (9 * tenthOfWidthX) && e.offsetX < halfOfWidthX + (10 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(14deg)";
+        //     }
+        // }
+
+        // else if(e.offsetX < halfOfWidthX)
+        // {
+        //     if(e.offsetX > 0 && e.offsetX < 1 * tenthOfWidthX)
+        //     {
+        //         e.target.style.transform = "rotateY(-5deg)";
+        //     }
+        //     if(e.offsetX > (1 * tenthOfWidthX) && e.offsetX < (2 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-6deg)";
+        //     }
+        //     if(e.offsetX > (2 * tenthOfWidthX) && e.offsetX < (3 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-7deg)";
+        //     }
+        //     if(e.offsetX > (3 * tenthOfWidthX) && e.offsetX < (4 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-8deg)";
+        //     }
+        //     if(e.offsetX > (4 * tenthOfWidthX) && e.offsetX < (5 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-9deg)";
+        //     }
+        //     if(e.offsetX > (5 * tenthOfWidthX) && e.offsetX < (6 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-10deg)";
+        //     }
+        //     if(e.offsetX > (6 * tenthOfWidthX) && e.offsetX < (7 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-11deg)";
+        //     }
+        //     if(e.offsetX > (7 * tenthOfWidthX) && e.offsetX < (8 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-12deg)";
+        //     }
+        //     if(e.offsetX > (8 * tenthOfWidthX) && e.offsetX < (9 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-13deg)";
+        //     }
+        //     if(e.offsetX > (9 * tenthOfWidthX) && e.offsetX < (10 * tenthOfWidthX))
+        //     {
+        //         e.target.style.transform = "rotateY(-14deg)";
+        //     }
+        // }
+
+        // if(e.offsetX > halfOfWidthY)
+        // {
+        //     if(e.offsetX > halfOfWidthY && e.offsetX < halfOfWidthY + (1 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(5deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (1 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (2 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(6deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (2 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (3 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(7deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (3 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (4 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(8deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (4 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (5 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(9deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (5 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (6 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(10deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (6 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (7 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(11deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (7 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (8 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(12deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (8 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (9 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(13deg)";
+        //     }
+        //     if(e.offsetX > halfOfWidthY + (9 * tenthOfWidthY) && e.offsetX < halfOfWidthY + (10 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(14deg)";
+        //     }
+        // }
+
+        // else if(e.offsetX < halfOfWidthY)
+        // {
+        //     if(e.offsetX > 0 && e.offsetX < 1 * tenthOfWidthY)
+        //     {
+        //         e.target.style.transform = "rotateX(-5deg)";
+        //     }
+        //     if(e.offsetX > (1 * tenthOfWidthY) && e.offsetX < (2 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-6deg)";
+        //     }
+        //     if(e.offsetX > (2 * tenthOfWidthY) && e.offsetX < (3 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-7deg)";
+        //     }
+        //     if(e.offsetX > (3 * tenthOfWidthY) && e.offsetX < (4 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-8deg)";
+        //     }
+        //     if(e.offsetX > (4 * tenthOfWidthY) && e.offsetX < (5 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-9deg)";
+        //     }
+        //     if(e.offsetX > (5 * tenthOfWidthY) && e.offsetX < (6 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-10deg)";
+        //     }
+        //     if(e.offsetX > (6 * tenthOfWidthY) && e.offsetX < (7 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-11deg)";
+        //     }
+        //     if(e.offsetX > (7 * tenthOfWidthY) && e.offsetX < (8 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-12deg)";
+        //     }
+        //     if(e.offsetX > (8 * tenthOfWidthY) && e.offsetX < (9 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-13deg)";
+        //     }
+        //     if(e.offsetX > (9 * tenthOfWidthY) && e.offsetX < (10 * tenthOfWidthY))
+        //     {
+        //         e.target.style.transform = "rotateX(-14deg)";
+        //     }
+        // }
+    });
+
+    card.addEventListener("mouseleave" , e => {
+        e.target.style.transform = "rotateY(0)";
+        e.target.style.transform = "rotateX(0)";
+    });
+});
+
+function _computeXRotation(e_offset, e_width) {
+    let deg = 15;
+    let half = e_width / 2;
+    let berechnungszahl;
+
+        berechnungszahl = half - e_offset;
+        console.log("Breite des EL:", e_width);
+        console.log("MausPOS:", berechnungszahl);
+        console.log("Davon Prozent:", (berechnungszahl * 100) / half);
+        
+        deg = deg * (berechnungszahl / 100);
+        console.log("Davon DEG:", deg);
+
+    return -deg;
+}
+
+function _computeYRotation(e_offset, e_height) {
+    let deg = 15;
+    let half = e_height / 2;
+    let berechnungszahl;
+
+    berechnungszahl = half - e_offset;
+console.clear();
+    console.log("MausPOS:", e_offset);
+    console.log("HEIGHT:", e_height);
+
+    deg = deg * (berechnungszahl / 100);
+
+    console.log("DEG:", deg);
+
+    return deg;
+}
